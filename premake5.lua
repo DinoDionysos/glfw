e1ae8ecaa949bfe1faf76962ce4fb28ffc3cbddb
DinoDionysos <91641294+DinoDionysos@users.noreply.github.com>
2022-03-30 17:49:30 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,29 +1,50 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-	staticruntime "off"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
+		--"include/GLFW/glfw3.h",
+		--"include/GLFW/glfw3native.h",
+		--"src/glfw_config.h",
+		--"src/context.c",
+		--"src/init.c",
+		--"src/input.c",
+		--"src/monitor.c",
+
+		--"src/null_init.c",
+		--"src/null_joystick.c",
+		--"src/null_monitor.c",
+		--"src/null_window.c",
+
+		--"src/platform.c",
+		--"src/vulkan.c",
+		--"src/window.c",
+
+		
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
 		"src/glfw_config.h",
+		"src/internal.h",
+    	"src/platform.h",
+    	"src/mappings.h",
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
-
-		"src/null_init.c",
-		"src/null_joystick.c",
-		"src/null_monitor.c",
-		"src/null_window.c",
-
 		"src/platform.c",
 		"src/vulkan.c",
 		"src/window.c",
+		"src/egl_context.c",
+
+		"src/null_init.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c"
 	}
 
 	filter "system:linux"
@@ -55,11 +76,24 @@ project "GLFW"
 
 		files
 		{
+			--"src/win32_init.c",
+			--"src/win32_joystick.c",
+			--"src/win32_module.c",
+			--"src/win32_monitor.c",
+			--"src/win32_time.c",
+			--"src/win32_thread.c",
+			--"src/win32_window.c",
+			--"src/wgl_context.c",
+			--"src/egl_context.c",
+			--"src/osmesa_context.c"
+
 			"src/win32_init.c",
-			"src/win32_joystick.c",
 			"src/win32_module.c",
+			"src/win32_joystick.c",
 			"src/win32_monitor.c",
+	 		"src/win32_time.h",
 			"src/win32_time.c",
+			"src/win32_thread.h",
 			"src/win32_thread.c",
 			"src/win32_window.c",
 			"src/wgl_context.c",
